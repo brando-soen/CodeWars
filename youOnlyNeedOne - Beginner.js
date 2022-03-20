139e364bfdf8c621983e1b273f55a785f2fa2a04
brando-soen <101693419+brando-soen@users.noreply.github.com>
2022-03-21 02:26:38 +0800

    You only need one - Beginner
    You only need one - Beginner

---
+++ youOnlyNeedOne - Beginner.js
@@ -0,0 +1,17 @@
+/* 8 kyu - You only need one - Beginner
+
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.
+*/
+
+// solution
+function check(a, x) {
+    if(a.indexOf(x) !== -1){
+      return true
+    } else{
+      return false
+    }
+  }
