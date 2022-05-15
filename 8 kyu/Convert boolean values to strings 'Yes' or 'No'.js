d24de5cc473b60a0c7af0effe9f239fb2fee6606
brando-soen <101693419+brando-soen@users.noreply.github.com>
2022-05-15 22:42:13 +0800

    Convert boolean values to strings 'Yes' or 'No'.
    Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.

---
+++ 8 kyu/Convert boolean values to strings 'Yes' or 'No'.js
@@ -0,0 +1,15 @@
+/* 8 kyu - Convert boolean values to strings 'Yes' or 'No'.
+
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+*/
+
+
+function boolToWord( bool ){
+    switch (bool){
+        case true: return "Yes";
+        case false: return "No";
+    }
+  }
+
+
