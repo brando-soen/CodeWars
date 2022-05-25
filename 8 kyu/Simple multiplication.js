f3c5f790d7209e42ba19e4d1c6dbd9a941f854d4
brando-soen <101693419+brando-soen@users.noreply.github.com>
2022-05-25 22:03:53 +0800

    Simple multiplication
    This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.

---
+++ 8 kyu/Simple multiplication.js
@@ -0,0 +1,15 @@
+/* 8 kyu - Simple multiplication
+
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+*/
+
+function simpleMultiplication(number) {
+    if (number%2 ==0){
+      return number*8
+    }
+  return number*9
+}
+
+
+
