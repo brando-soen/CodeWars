166eafe567b8eedb5a8eb192adeb4de69cf3cd01
brando-soen <101693419+brando-soen@users.noreply.github.com>
2022-07-05 22:56:06 +0800

    Sum Mixed Array
    Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
    
    Return your answer as a number.

---
+++ 8 kyu/Sum Mixed Array.js
@@ -0,0 +1,17 @@
+/* 8 kyu - Sum Mixed Array
+
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+
+
+*/
+
+function sumMix(x){
+    let sum = 0
+    
+    for (let i=0;i<x.length;i++){
+      sum += parseInt(x[i])
+    }
+     return sum
+   }
